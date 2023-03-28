d34f8f9aa0244e8714946918613ed9c459601fe9
Camille Arce <thecamillearce@gmail.com>
2023-03-28 20:54:13 +0000

    initial commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+
+# the get method expects a String as the first argument, and then a hash as a second argument where it tells which "action" or method to execute when someone visits the path
+
+class ApplicationController < ActionController::Base
+  def play_rock
+    self.redirect_to("https://www.wikipedia.org")
+  end
+end
