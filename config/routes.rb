cd07c88b584480b906139e5e9390d031ba7dcbdc
Camille Arce <thecamillearce@gmail.com>
2023-04-13 04:16:05 +0000

    finished assignment

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
 end
 
 # the get method expects a String as the first argument, and then a hash as a second argument where it tells which "action" or method to execute when someone visits the path
